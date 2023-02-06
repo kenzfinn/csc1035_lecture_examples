2d8169fa09305d1e4ace7b7b7e7936a47c471dde
kenzfinn <113994029+kenzfinn@users.noreply.github.com>
2023-02-06 15:32:51 +0000

    put classes into a package

--- src/edu/ncl/csc1035/java101/BrokenWorld.java
+++ src/edu/ncl/csc1035/java101/BrokenWorld.java
@@ -1,3 +1,5 @@
+package edu.ncl.csc1035.java101;
+
 public class BrokenWorld {
     public static void main(String[] args) {
         //System.out.printl("Hello World");
--- src/edu/ncl/csc1035/java101/HelloWorld.java
+++ src/edu/ncl/csc1035/java101/HelloWorld.java
@@ -1,3 +1,5 @@
+package edu.ncl.csc1035.java101;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
