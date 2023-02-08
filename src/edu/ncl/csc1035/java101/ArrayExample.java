a74b7690b7fe2ccb3989d899139deef1f123c81b
kenzfinn <113994029+kenzfinn@users.noreply.github.com>
2023-02-08 16:36:14 +0000

    added array example class

---
+++ src/edu/ncl/csc1035/java101/ArrayExample.java
@@ -0,0 +1,9 @@
+package edu.ncl.csc1035.java101;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] numbers= {1,2,4,5,3,3,67};
+        System.out.println(numbers[2]);
+        System.out.println(numbers.length);
+    }
+}
